f070b158c9de1f59a5c72b95658f1cb1600dc166
JelaJs <nikola4434@gmail.com>
2025-01-15 21:23:44 +0100

    middleware for auth, and role - group routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminCheckMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::view("/about", "about");
@@ -13,20 +14,27 @@ Route::get("/", [HomepageController::class,"index"]);
 Route::get("/contact", [ContactController::class,"index"]);
 Route::get("/shop", [ShopController::class,"index"]);
 
-Route::get("/admin/all-contacts", [ContactController::class,"getAllContacts"])->name("allContacts");
-Route::get("/admin/delete-contact/{contact}", [ContactController::class,"delete"])->name("deleteContact");
-Route::get("/admin/edit-contact-form/{contact}", [ContactController::class,"editView"])->name("editContactView");
-Route::post("/admin/edit-contact", [ContactController::class,"edit"])->name("editContact");
+Route::middleware(['auth', AdminCheckMiddleware::class])->prefix("admin")->group(function () { 
 
-Route::get("/admin/add-product", [ShopController::class,"productForm"]);
-Route::get("/admin/products", [ProductsController::class,"index"])->name("allProducts");
-Route::get("/admin/delete-product/{product}", [ProductsController::class,"delete"])->name("deleteProduct");
-Route::get("/admin/edit-product/{product}", [ProductsController::class,"editView"])->name("editProductView");
-Route::post("/admin/edit-product/{product}", [ProductsController::class,"edit"])->name("editProduct");
+    Route::get("/all-contacts", [ContactController::class,"getAllContacts"])->name("allContacts");
+    Route::get("/delete-contact/{contact}", [ContactController::class,"delete"])->name("deleteContact");
+    Route::get("/edit-contact-form/{contact}", [ContactController::class,"editView"])->name("editContactView");
+    Route::post("/edit-contact", [ContactController::class,"edit"])->name("editContact");
+    
+    Route::get("/add-product", [ShopController::class,"productForm"]);
+    Route::get("/products", [ProductsController::class,"index"])->middleware('auth')->name("allProducts");
+    Route::get("/delete-product/{product}", [ProductsController::class,"delete"])->name("deleteProduct");
+    Route::get("/edit-product/{product}", [ProductsController::class,"editView"])->name("editProductView");
+    Route::post("/edit-product/{product}", [ProductsController::class,"edit"])->name("editProduct");
+
+});
+
+
 
 Route::post("/send-message", [ContactController::class,"sendMessage"]);
 Route::post("/add-product", [ProductsController::class,"addProduct"])->name('addProduct');
 
+/////////////////////////////////////////////////////////////////////
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
