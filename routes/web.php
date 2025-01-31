9b164ee7866fe1cbb3dffc1216d2a1ebc2c2ff29
JelaJs <nikola4434@gmail.com>
2025-01-31 21:54:37 +0100

    Grouping prefixes

--- routes/web.php
+++ routes/web.php
@@ -24,19 +24,19 @@ Route::get("/shop", [ShopController::class,"index"]);
 ///ADMIN
 Route::middleware(['auth', AdminCheckMiddleware::class])->prefix("admin")->group(function () { 
 
-    Route::controller(ContactController::class)->group(function () {
-        Route::get("/all-contacts", "getAllContacts")->name("allContacts");
-        Route::get("/delete-contact/{contact}", "delete")->name("deleteContact");
-        Route::get("/edit-contact-form/{contact}", "editView")->name("editContactView");
-        Route::post("/edit-contact", "edit")->name("editContact");
+    Route::controller(ContactController::class)->prefix('/contacts')->group(function () {
+        Route::get("/all", "getAllContacts")->name("allContacts");
+        Route::get("/delete/{contact}", "delete")->name("deleteContact");
+        Route::get("/edit-form/{contact}", "editView")->name("editContactView");
+        Route::post("/edit", "edit")->name("editContact");
     });
 
    
-    Route::controller(ProductsController::class)->group(function () { 
-        Route::get("/products", "index")->name("allProducts");
-        Route::get("/delete-product/{product}", "delete")->name("deleteProduct");
-        Route::get("/edit-product/{product}", "editView")->name("editProductView");
-        Route::post("/edit-product/{product}", "edit")->name("editProduct");
+    Route::controller(ProductsController::class)->prefix('products')->group(function () { 
+        Route::get("/all", "index")->name("allProducts");
+        Route::get("/delete/{product}", "delete")->name("deleteProduct");
+        Route::get("/edit/{product}", "editView")->name("editProductView");
+        Route::post("/edit/{product}", "edit")->name("editProduct");
     });
     
     Route::get("/add-product", [ShopController::class,"productForm"]);
