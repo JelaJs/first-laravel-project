2f3e5be1643e246685c1e28bd2d4d0a1b1701381
JelaJs <nikola4434@gmail.com>
2025-02-04 16:05:29 +0100

    orders, orderItems tables and models

---
+++ app/Models/OrderItems.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItems extends Model
+{
+    protected $table = 'orderItems';
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'amount',
+        'price'
+    ];
+}
